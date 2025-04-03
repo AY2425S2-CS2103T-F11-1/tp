86d9fca8f97f744c0989a9af18c3bfa27c019c7a
reaneechua <reaneechua@gmail.com>
2025-04-03 21:40:59 +0800

    Update junit tests for AddApptCommandParser

--- src/test/java/seedu/address/logic/parser/AddApptCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddApptCommandParserTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,49 +14,41 @@ public class AddApptCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsAddApptCommand() throws Exception {
-        // valid input with no preamble and one occurrence of each required prefix
-        String validInput = " -IC S1234567A -D 25/12/2025 14:30";
-        AddApptCommand expectedCommand = new AddApptCommand("S1234567A", "25/12/2025 14:30");
-        AddApptCommand result = parser.parse(validInput);
-        assertEquals(expectedCommand, result);
+        String nric = "S1234567A";
+        String date = "01/05/2025 14:30";
+        String userInput = " -IC " + nric + " -D " + date;
+
+        AddApptCommand command = parser.parse(userInput);
+        assertEquals(new AddApptCommand(nric, date), command);
     }
 
     @Test
     public void parse_missingNricPrefix_throwsParseException() {
-        // missing NRIC prefix, only date provided
-        String inputMissingNric = " -D 25/12/2025 14:30";
-        assertThrows(ParseException.class, () -> parser.parse(inputMissingNric));
+        String userInput = " -D 01/05/2025 14:30";
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
     }
 
     @Test
     public void parse_missingDatePrefix_throwsParseException() {
-        // missing date prefix, only NRIC provided
-        String inputMissingDate = " -IC S1234567A";
-        assertThrows(ParseException.class, () -> parser.parse(inputMissingDate));
+        String userInput = " -IC S1234567A";
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
     }
 
     @Test
-    public void parse_nonEmptyPreamble_throwsParseException() {
-        // preamble should be empty, but a stray word is provided at the start
-        String inputWithPreamble = "extra -IC S1234567A -D 25/12/2025 14:30";
-        ParseException exception = assertThrows(ParseException.class, () -> parser.parse(inputWithPreamble));
-        // Optionally, check that the error message matches the expected invalid command format.
-        assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                AddApptCommand.MESSAGE_USAGE), exception.getMessage());
+    public void parse_invalidDateFormat_throwsParseException() {
+        String userInput = " -IC S1234567A -D 2025-05-01";
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
     }
 
     @Test
-    public void parse_duplicateNricPrefix_throwsParseException() {
-        // duplicate NRIC prefixes provided should cause an exception.
-        String inputWithDuplicateNric = " -IC S1234567A -IC S7654321B -D 25/12/2025 14:30";
-        assertThrows(ParseException.class, () -> parser.parse(inputWithDuplicateNric));
+    public void parse_duplicatePrefixes_throwsParseException() {
+        String userInput = " -IC S1234567A -IC S7654321B -D 01/05/2025 14:30";
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
     }
 
     @Test
-    public void parse_duplicateDatePrefix_throwsParseException() {
-        // duplicate date prefixes provided should cause an exception.
-        String inputWithDuplicateDate = " -IC S1234567A -D 25/12/2025 14:30 -D 26/12/2025 10:00";
-        assertThrows(ParseException.class, () -> parser.parse(inputWithDuplicateDate));
+    public void parse_extraPreamble_throwsParseException() {
+        String userInput = "extra -IC S1234567A -D 01/05/2025 14:30";
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
     }
 }
-
